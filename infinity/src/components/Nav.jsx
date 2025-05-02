600a49260512d9d6980dd6b61c46f4f3b470cc3f
eliamuzu <kofiprince375@gmail.com>
2025-05-02 09:52:14 +0000

    Deletion of duplicate files and unused styles. Update to Nav and Home components.

--- infinity/src/components/Nav.jsx
+++ infinity/src/components/Nav.jsx
@@ -1,26 +1,46 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
-import { IoIosMenu } from "react-icons/io";
+import { IoIosMenu,IoIosInfinite } from "react-icons/io";
 
 
 function Nav() {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+
   return (
-   <nav>
-     <div>
-        <img src="" alt="infinity logo" />
-        <span>Infinity Dental and Eye center</span>
+   <nav className='flex justify-between mt-1 p-2 ml-2 mr-2 '>
+     <div className='text-center lg:flex gap-3'>
+        <IoIosInfinite className='text-3xl place-self-center'/>
+        <span className='place-self-center lg:text-xl font-medium'>Infinity Dental and Eye Center</span>
      </div>
 
-     <div className='hidden md:flex'>
-        <ul>
+     <div className='hidden md:inline p-3'>
+        <ul className='flex gap-7'>
             <Link>About Us</Link>
             <Link>Services</Link>
+            <Link>Contact</Link>
             <Link>Book an Appointment</Link>
         </ul>
      </div>
 
-     <div>
-        <button><IoIosMenu /></button>
+     <div className='md:hidden p-3'>
+        <button onClick={toggleMenu} className='text-3xl cursor-pointer opacity-70'>
+            <IoIosMenu />
+        </button>
+        {isOpen && (
+          <div className='h-full absolute right-5 bg-white shadow-lg rounded-lg z-10'>
+            <ul className='text-xl flex flex-col gap-3 p-4'>
+              <Link to="/about" onClick={() => setIsOpen(false)}>About Us</Link>
+              <Link to="/services" onClick={() => setIsOpen(false)}>Services</Link>
+              <Link to="/contact" onClick={() => setIsOpen(false)}>Contact</Link>
+              <Link to="/appointment" onClick={() => setIsOpen(false)}>Book an Appointment</Link>
+            </ul>
+          </div>
+        )}
      </div>
    </nav>
     
--- infinity/src/pages/Home.jsx
+++ infinity/src/pages/Home.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import { RiToothLine } from "react-icons/ri";
+import { GiSunglasses } from "react-icons/gi";
 
 
 
 function Home() {
   return (
-    null
+    <div className='h-screen'>
+
+      <span>
+        <p className='text-3xl text-center mt-5 md:text-left '>PROVIDING
+          <em className='font-light'> THE BEST</em><br/> DENTAL AND EYE CARE
+        </p>
+      </span>
+
+      <div className='h-90 relative'>
+        <div className='relative flex mt-10 items-center justify-center md:'>
+          <img src="src/images/homebg.jpg" alt="" className='absolute md:relative md:w-1/3 top-0 left-5 w-3/5 rounded-3xl'/>
+          <span className='text-5xl opacity-60'><RiToothLine className='absolute md:relative top-10 right-15 '/> </span>
+          <img src="src/images/homebg.jpg" alt="" className='absolute md:hidden top-40 right-5 w-3/5 rounded-3xl'/>
+          <span className='absolute top-55 left-10 text-6xl opacity-80 '><GiSunglasses /></span>
+        </div>
+      </div>
+
+      <p className='text-justify text-xl ml-6 mr-6'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, officiis doloremque iure laborum quidem excepturi deleniti. Voluptates corrupti molestias perferendis ea dignissimos qui. Ex reprehenderit, reiciendis consequatur blanditiis sequi enim?
+      </p>
+
+      <div className='border-2 text-2xl w-4/5 text-center place-self-center rounded-2xl mt-5'>
+        <button className='p-2'>Book an Appointment</button>
+      </div>
+
+    </div>
   )
 }
 
